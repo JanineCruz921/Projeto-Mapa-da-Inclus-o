16d0f54613aecbf363f1d5ff2102f408c9449f1f
JanineCruz921 <cruzjanine921@gmail.com>
2024-04-03 23:02:04 -0300

    editing some test information

--- mapa da inclusão/src/components/small components/card.jsx
+++ mapa da inclusão/src/components/small components/card.jsx
@@ -1,8 +1,8 @@
 function card() {
     return (
         <div class="pages-card">
-            <h1 id="h1-data">hello craziness</h1>
-            <li id="li-data">uma lista</li>
+            <h1 id="h1-data">hello world</h1>
+            <li id="li-data">a list here</li>
         </div>
     )
 }
