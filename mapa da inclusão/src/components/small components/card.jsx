255fc0961c4f6a00e53429c601a18d9877914419
JanineCruz921 <cruzjanine921@gmail.com>
2024-04-03 22:53:12 -0300

    uploading data and server reconfigured

---
+++ mapa da inclusão/src/components/small components/card.jsx
@@ -0,0 +1,10 @@
+function card() {
+    return (
+        <div class="pages-card">
+            <h1 id="h1-data">hello craziness</h1>
+            <li id="li-data">uma lista</li>
+        </div>
+    )
+}
+
+export default card
---
+++ mapa da inclusão/src/components/small components/navBar.jsx
@@ -0,0 +1,12 @@
+function navBar(){
+    return (
+    <navBar>
+        <div id="logo">logo img</div>
+        <div id="ongs">ongs</div>
+        <div id="mapa">mapa</div>
+        <div id="deficiencias">deficiências cadastradas</div>
+    </navBar>
+);
+}
+
+export default navBar
